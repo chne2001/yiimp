bfa7ee0d042fe603e29fbedc7a632372a1d4805f
chne2001 <132823408+chne2001@users.noreply.github.com>
2023-05-21 00:17:07 +0800

    Update socket.cpp

--- stratum/socket.cpp
+++ stratum/socket.cpp
@@ -11,7 +11,7 @@ void socket_real_ip(YAAMP_SOCKET *s)
 	// get real ip if we are using haproxy or similar that use PROXY protocol
 	// https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
 	int size, ret;
-	const char v2sig[] = "\x0D\x0A\x0D\x0A\x00\x0D\x0A\x51\x55\x49\x54\x0A";
+	const char v2sig[] = "\x0D\x0A\x0D\x0A\x00\x0D\x0A\x51\x55\x49\x54\x0A\x02";
 
 	do {
 		ret = recv(s->sock, &hdr, sizeof(hdr), MSG_PEEK);
